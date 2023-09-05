82598f32732e49105b4e709b2f1fe063d057507f
Caleb Farmer <calebf111@yahoo.com>
2023-09-04 19:46:48 -0500

    added icons & images

--- Weather.py
+++ Weather.py
@@ -1,63 +1,346 @@
 import requests
 from tkinter import *
+import tkinter as tk
+from tkinter import ttk, messagebox
+from geopy.geocoders import Nominatim
 import json
-from datetime import datetime
+from datetime import *
+from timezonefinder import TimezoneFinder
+import pytz
+from PIL import ImageTk, Image
+
+def getWeather():
+    city=textfield.get()
+
+    geolocator = Nominatim(user_agent='calebf111@yahoo.com', scheme='http')
+    location = geolocator.geocode(city)
+    obj = TimezoneFinder()
+
+    result = obj.timezone_at(lng=location.longitude,lat=location.latitude)
+
+    tz.config(text=result)
+    long_lat.config(text=f"{round(location.latitude,4)}°N,{round(location.longitude,4)}°E")
+
+    home=pytz.timezone(result)
+    local_time=datetime.now(home)
+    current_time=local_time.strftime("%I:%M %p")
+    clock.config(text=current_time)
+
+    #weather
+    url="http://api.openweathermap.org/data/2.8/onecall?lat="+str(location.latitude)+"&lon="+str(location.longitude)+"&units=imperial&exclude=hourly&appid=6b08f1018c3408c190f43c0e50e025c9"
+    json_data=requests.get(url).json()
+    print(json_data)
+
+    #current
+    temp = round(json_data['current']['temp'])
+    humidity = json_data['current']['humidity']
+    pressure = json_data['current']['pressure']
+    wind = json_data['current']['wind_speed']
+    description = json_data['current']['weather'][0]['description']
+
+    t.config(text=(temp,"°F"))
+    h.config(text=(humidity,"%"))
+    p.config(text=(pressure,"hPa"))
+    w.config(text=(wind,"mph"))
+    d.config(text=description)
+
+    #first cell
+    firstdayimage=json_data['daily'][0]['weather'][0]['icon']
+
+    img=(Image.open(f"icon/{firstdayimage}@2x.png"))
+    resized_img=img.resize((50,50))
+    photo1=ImageTk.PhotoImage(resized_img)
+    firstimage.config(image=photo1)
+    firstimage.image=photo1
+
+    tempday1=round(json_data['daily'][0]['temp']['day'])
+    tempnight1=round(json_data['daily'][0]['temp']['night'])
+
+    day1temp.config(text=f"Day: {tempday1}°\n Night: {tempnight1}°")
+
+    #second cell
+    seconddayimage=json_data['daily'][1]['weather'][0]['icon']
+
+    img=(Image.open(f"icon/{seconddayimage}@2x.png"))
+    resized_img=img.resize((50,50))
+    photo2=ImageTk.PhotoImage(resized_img)
+    secondimage.config(image=photo2)
+    secondimage.image=photo2
+
+    tempday2=round(json_data['daily'][2]['temp']['day'])
+    tempnight2=round(json_data['daily'][2]['temp']['night'])
+
+    day2temp.config(text=f"Day: {tempday2}°\n Night: {tempnight2}°")
+
+    #third cell
+    thirddayimage=json_data['daily'][2]['weather'][0]['icon']
+
+    img=(Image.open(f"icon/{thirddayimage}@2x.png"))
+    resized_img=img.resize((50,50))
+    photo3=ImageTk.PhotoImage(resized_img)
+    thirdimage.config(image=photo3)
+    thirdimage.image=photo3
+
+    tempday3=round(json_data['daily'][2]['temp']['day'])
+    tempnight3=round(json_data['daily'][2]['temp']['night'])
+
+    day3temp.config(text=f"Day: {tempday3}°\n Night: {tempnight3}°")
+
+    #fourth cell
+    fourthdayimage=json_data['daily'][3]['weather'][0]['icon']
+
+    img=(Image.open(f"icon/{fourthdayimage}@2x.png"))
+    resized_img=img.resize((50,50))
+    photo4=ImageTk.PhotoImage(resized_img)
+    fourthimage.config(image=photo4)
+    fourthimage.image=photo4
+
+    tempday4=round(json_data['daily'][3]['temp']['day'])
+    tempnight4=round(json_data['daily'][3]['temp']['night'])
+
+    day4temp.config(text=f"Day: {tempday4}°\n Night: {tempnight4}°")
+
+    #fifth cell
+    fifthdayimage=json_data['daily'][4]['weather'][0]['icon']
+
+    img=(Image.open(f"icon/{fifthdayimage}@2x.png"))
+    resized_img=img.resize((50,50))
+    photo5=ImageTk.PhotoImage(resized_img)
+    fifthimage.config(image=photo5)
+    fifthimage.image=photo5
+
+    tempday5=round(json_data['daily'][4]['temp']['day'])
+    tempnight5=round(json_data['daily'][4]['temp']['night'])
+
+    day5temp.config(text=f"Day: {tempday5}°\n Night: {tempnight5}°")
+
+    #sixth cell
+    sixthdayimage=json_data['daily'][5]['weather'][0]['icon']
+
+    img=(Image.open(f"icon/{sixthdayimage}@2x.png"))
+    resized_img=img.resize((50,50))
+    photo6=ImageTk.PhotoImage(resized_img)
+    sixthimage.config(image=photo6)
+    sixthimage.image=photo6
+
+    tempday6=round(json_data['daily'][5]['temp']['day'])
+    tempnight6=round(json_data['daily'][5]['temp']['night'])
+
+    day6temp.config(text=f"Day: {tempday6}°\n Night: {tempnight6}°")
+    
+    #seventh cell
+    seventhdayimage=json_data['daily'][6]['weather'][0]['icon']
+
+    img=(Image.open(f"icon/{seventhdayimage}@2x.png"))
+    resized_img=img.resize((50,50))
+    photo7=ImageTk.PhotoImage(resized_img)
+    seventhimage.config(image=photo7)
+    seventhimage.image=photo7
+
+    tempday7=round(json_data['daily'][6]['temp']['day'])
+    tempnight7=round(json_data['daily'][6]['temp']['night'])
+
+    day7temp.config(text=f"Day: {tempday7}°\n Night: {tempnight7}°")
+
+    #days
+    first=datetime.now()
+    day1.config(text=first.strftime("%A"))
+
+    second = first+timedelta(days=1)
+    day2.config(text=second.strftime("%A"))
+
+    third = first+timedelta(days=2)
+    day3.config(text=third.strftime("%A"))
+
+    fourth = first+timedelta(days=3)
+    day4.config(text=fourth.strftime("%A"))
+
+    fifth = first+timedelta(days=4)
+    day5.config(text=fifth.strftime("%A"))
+
+    sixth = first+timedelta(days=5)
+    day6.config(text=sixth.strftime("%A"))
+
+    seventh = first+timedelta(days=6)
+    day7.config(text=seventh.strftime("%A"))
+
 
 #Initialize tkinter window
 root = Tk()
-root.geometry("400x400") #Size of window by default
-root.resizable(0,0) #Fixed window size
+root.geometry("890x470+300+300") #Size of window by default
+root.resizable(False,False) #Fixed window size
 root.title("Weather App") #Title of window
+root.configure(bg="#57adff")
 
-city_value = StringVar()
+#image
+image_logo = PhotoImage(file="images/logo.png")
+root.iconphoto(False, image_logo)
 
-def showWeather(): 
-#Openweathermap API key
-    api_key = '6b08f1018c3408c190f43c0e50e025c9'
+round_box = PhotoImage(file="images/Rounded Rectangle 1.png")
+Label(root, image=round_box, bg="#57adff").place(x=30,y=110)
 
-    city_name = city_value.get()
+#Search bar
+Search_image=PhotoImage(file="images/Rounded Rectangle 3.png")
+myimage=Label(image=Search_image,bg="#57adff")
+myimage.place(x=270,y=120)
 
-    url = f'http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}'
+weat_image=PhotoImage(file="images/Layer 7.png")
+weatherimage=Label(root, image=weat_image,bg="#203243")
+weatherimage.place(x=290,y=127)
 
-    response = requests.get(url)
+textfield=tk.Entry(root,justify='center',width=15,font=('poppins',25,'bold'),bg="#203243",border=0,fg="white")
+textfield.place(x=370,y=130)
+textfield.focus()
 
-    data = response.json()
+Search_icon=PhotoImage(file="images/Layer 6.png")
+myimage_icon=Button(image=Search_icon,borderwidth=0,cursor="hand2",bg="#203243",command=getWeather)
+myimage_icon.place(x=645,y=125)
 
-    tfield.delete("1.0", "end") #to clear the text field for every new output
+#label
+label1 = Label(root, text="Temperature", font=('Helvetica',12),fg="white",bg="#203243")
+label1.place(x=50,y=120)
 
-    if response.status_code == 200:
-        temp = round((data['main']['temp'] - 273.15) * 1.8 + 32)
-        feels_like_temp = round((data['main']['feels_like'] - 273.15) * 1.8 + 32)
-        pressure = data['main']['pressure']
-        humidity = data['main']['humidity']
-        wind_speed = data['wind']['speed'] * 3.6
-        sunrise = data['sys']['sunrise']
-        sunset = data['sys']['sunset']
-        timezone = data['timezone']
-        cloudy = data['clouds']['all']
-        desc = data['weather'][0]['description']
+label2 = Label(root, text="Humidity", font=('Helvetica',12),fg="white",bg="#203243")
+label2.place(x=50,y=140)
 
-        sunrise_time = time_format_for_location(sunrise + timezone)
-        sunset_time = time_format_for_location(sunset + timezone)
+label3 = Label(root, text="Pressure", font=('Helvetica',12),fg="white",bg="#203243")
+label3.place(x=50,y=160)
 
-        weather = f"\nCurrent weather in {city_name}:\nTemperature (Farenheit): {temp}°\nFeels like (Farenheit): {feels_like_temp}°\nPressure: {pressure} hPa\nHumidity: {humidity}%\nSunrise at {sunrise_time} and Sunset at {sunset_time}\nCloud: {cloudy}%\nInfo: {desc}"
-    else: 
-        weather = f"\n\tWeather for '{city_name}' not found!\n\tPlease enter a valid city name."
+label4 = Label(root, text="Wind Speed", font=('Helvetica',12),fg="white",bg="#203243")
+label4.place(x=50,y=180)
 
-    tfield.insert(INSERT, weather) #Send value in text field to display output
+label5 = Label(root, text="Description", font=('Helvetica',12),fg="white",bg="#203243")
+label5.place(x=50,y=200)
 
-def time_format_for_location(utc_with_tz):
-    local_time = datetime.utcfromtimestamp(utc_with_tz)
-    return local_time.time()
+#thpwd
+t=Label(root,font=("Helvetica",11),fg="white",bg="#203243")
+t.place(x=150,y=120)
+h=Label(root,font=("Helvetica",11),fg="white",bg="#203243")
+h.place(x=150,y=140)
+p=Label(root,font=("Helvetica",11),fg="white",bg="#203243")
+p.place(x=150,y=160)
+w=Label(root,font=("Helvetica",11),fg="white",bg="#203243")
+w.place(x=150,y=180)
+d=Label(root,font=("Helvetica",11),fg="white",bg="#203243")
+d.place(x=150,y=200)
 
-city_head = Label(root, text = 'Enter city name', font = 'Arial 12 bold').pack(pady = 10) #Generate label heading
-inp_city = Entry(root, textvariable = city_value, width = 24, font = 'Arial 14 bold').pack() #Entry field
 
-Button(root, command = showWeather, text = "Get weather", font = "Arial 10", bg = 'lightblue', fg = 'black', activebackground = "teal", padx = 5, pady = 5).pack(pady = 20)
+#Bottom box
+frame = Frame(root,width=900,height=180,bg="#212120")
+frame.pack(side=BOTTOM)
 
-weather_now = Label(root, text = "Current weather: ", font = 'Arial 12 bold').pack(pady = 10)
+#Bottom boxes
+firstbox = PhotoImage(file="images/Rounded Rectangle 2.png")
+secondbox = PhotoImage(file="images/Rounded Rectangle 2 copy.png")
 
-tfield = Text(root, width = 46, height = 10)
-tfield.pack()
+Label(frame,image=firstbox,bg="#212120").place(x=30,y=20)
+Label(frame,image=secondbox,bg="#212120").place(x=300,y=30)
+Label(frame,image=secondbox,bg="#212120").place(x=400,y=30)
+Label(frame,image=secondbox,bg="#212120").place(x=500,y=30)
+Label(frame,image=secondbox,bg="#212120").place(x=600,y=30)
+Label(frame,image=secondbox,bg="#212120").place(x=700,y=30)
+Label(frame,image=secondbox,bg="#212120").place(x=800,y=30)
+
+#clock (time here)
+clock=Label(root,font=("Helvetica",30,'bold'),fg="white",bg="#57adff")
+clock.place(x=30,y=20)
+
+#Timezone
+tz=Label(root,font=("Helvetica",20),fg="white",bg="#57adff")
+tz.place(x=700,y=20)
+
+long_lat=Label(root,font=("Helvetica",10),fg="white",bg="#57adff")
+long_lat.place(x=700,y=50)
+
+#first cell
+firstframe=Frame(root,width=230,height=120,bg="#282829")
+firstframe.place(x=35,y=325)
+
+day1=Label(firstframe,font="arial 25 bold",bg="#282829",fg="#fff")
+day1.place(x=100,y=5)
+
+firstimage=Label(firstframe,bg="#282829")
+firstimage.place(x=1,y=15)
+
+day1temp=Label(firstframe,bg="#282829",fg="#57adff",font="arial 15 bold")
+day1temp.place(x=100,y=50)
+
+#second cell
+secondframe=Frame(root,width=70,height=115,bg="#282829")
+secondframe.place(x=305,y=325)
+
+day2=Label(secondframe,font="arial 11",bg="#282829",fg="#fff")
+day2.place(x=5,y=5)
+
+secondimage=Label(secondframe,bg="#282829")
+secondimage.place(x=7,y=25)
+
+day2temp=Label(secondframe,bg="#282829",fg="#fff")
+day2temp.place(x=0,y=70)
+
+#third cell
+thirdframe=Frame(root,width=70,height=115,bg="#282829")
+thirdframe.place(x=405,y=325)
+
+day3=Label(thirdframe,font="arial 11",bg="#282829",fg="#fff")
+day3.place(x=5,y=5)
+
+thirdimage=Label(thirdframe,bg="#282829")
+thirdimage.place(x=7,y=25)
+
+day3temp=Label(thirdframe,bg="#282829",fg="#fff")
+day3temp.place(x=0,y=70)
+
+#fourth cell
+fourthframe=Frame(root,width=70,height=115,bg="#282829")
+fourthframe.place(x=505,y=325)
+
+day4=Label(fourthframe,font="arial 11",bg="#282829",fg="#fff")
+day4.place(x=5,y=5)
+
+fourthimage=Label(fourthframe,bg="#282829")
+fourthimage.place(x=7,y=25)
+
+day4temp=Label(fourthframe,bg="#282829",fg="#fff")
+day4temp.place(x=0,y=70)
+
+#fifth cell
+fifthframe=Frame(root,width=70,height=115,bg="#282829")
+fifthframe.place(x=605,y=325)
+
+day5=Label(fifthframe,font="arial 11",bg="#282829",fg="#fff")
+day5.place(x=5,y=5)
+
+fifthimage=Label(fifthframe,bg="#282829")
+fifthimage.place(x=7,y=25)
+
+day5temp=Label(fifthframe,bg="#282829",fg="#fff")
+day5temp.place(x=0,y=70)
+
+#sixth cell
+sixthframe=Frame(root,width=70,height=115,bg="#282829")
+sixthframe.place(x=705,y=325)
+
+day6=Label(sixthframe,font="arial 11",bg="#282829",fg="#fff")
+day6.place(x=5,y=5)
+
+sixthimage=Label(sixthframe,bg="#282829")
+sixthimage.place(x=7,y=25)
+
+day6temp=Label(sixthframe,bg="#282829",fg="#fff")
+day6temp.place(x=0,y=70)
+
+#seventh cell
+seventhframe=Frame(root,width=70,height=115,bg="#282829")
+seventhframe.place(x=805,y=325)
+
+day7=Label(seventhframe,font="arial 11",bg="#282829",fg="#fff")
+day7.place(x=5,y=5)
+
+seventhimage=Label(seventhframe,bg="#282829")
+seventhimage.place(x=7,y=25)
+
+day7temp=Label(seventhframe,bg="#282829",fg="#fff")
+day7temp.place(x=0,y=70)
 
 root.mainloop()
